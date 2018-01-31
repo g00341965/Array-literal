248fcfd96b95cdcf42c57bb4beab7bc724d7bf93
g00341965 <35809401+g00341965@users.noreply.github.com>
2018-01-31 13:53:36 +0000

    Add files via upload

--- array-literal.js
+++ array-literal.js
@@ -1,7 +1,13 @@
 // Create an array and assign it values.
 var colors;
-colors = ['white', 'black', 'red'];
+colors = ['white', 'black', 'beige'];
 
 // Show the first item from the array 
 var el = document.getElementById('colors');
 el.textContent = colors[2];
+
+// Update the third item in the array
+colors[2] = 'beige';
+
+
+
